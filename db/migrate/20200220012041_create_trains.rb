82559311a91594a83cff6ad9ee0fecccc555fe4d
Fei Wang <feiwangsioc@gmail.com>
2020-02-19 22:29:50 -0500

    api done

---
+++ db/migrate/20200220012041_create_trains.rb
@@ -0,0 +1,10 @@
+class CreateTrains < ActiveRecord::Migration[5.2]
+  def change
+    create_table :trains do |t|
+
+      t.string :number, :index => true 
+
+      t.timestamps
+    end
+  end
+end
