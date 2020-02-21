71d975c06e83946b0e863e8874500b4f58ad16df
Fei Wang <feiwangsioc@gmail.com>
2020-02-20 23:09:46 -0500

    api

--- Gemfile
+++ Gemfile
@@ -24,6 +24,14 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 
 gem 'rest-client'
+
+gem 'devise'
+
+gem 'carrierwave'
+
+gem 'mini_magick'
+
+gem 'will_paginate'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use ActiveModel has_secure_password
---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+
+	mount_uploader :avatar, AvatarUploader
+
+	has_many :reservations
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  before_create :generate_authentication_token
+
+    def generate_authentication_token
+      self.authentication_token = Devise.friendly_token
+    end
+
+end
